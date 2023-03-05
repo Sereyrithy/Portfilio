c73b4406a92169060ad3f6b4345930293ff0fa52
Sereyrithy Vandy <vandysereyrithyy@gmail.com>
2023-03-05 13:49:02 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <></>
+    <>
+      <Navbar/>
+
+       <main className='main'>
+          <Home/>
+       </main>
+    </>
   );
 }
 

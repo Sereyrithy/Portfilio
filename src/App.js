589054eeca578205eeae735faa0305b2080c812b
Sereyrithy Vandy <vandysereyrithyy@gmail.com>
2023-03-06 11:23:09 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/home/Home';
+import About from './components/about/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
        <main className='main'>
           <Home/>
+          <About/>
        </main>
     </>
   );
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="#" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i></a>
+        <a href="https://www.instagram.com/invites/contact/?i=ks2e90m2x5g&utm_content=6mbolpn" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i></a>
 
-        <a href="#" className="home__social-icon" target="_blank"><i class="uil uil-facebook-f"></i></a>
+        <a href="https://www.facebook.com/Rithyhen007?mibextid=ZbWKwL" className="home__social-icon" target="_blank"><i class="uil uil-facebook-f"></i></a>
 
-        <a href="#" className="home__social-icon" target="_blank"><i class="uil uil-github-alt"></i></a>
+        <a href="https://github.com/Sereyrithy" className="home__social-icon" target="_blank"><i class="uil uil-github-alt"></i></a>
     </div>
   )
 }
